1a893e0493776d1df49d7730de3a37cd19310484
Oleksandr Koval <alexsanyakoval@gmail.com>
2023-09-25 20:39:25 -0500

    finished integration test

--- test/Integration.t.sol
+++ test/Integration.t.sol
@@ -4,7 +4,9 @@ import "forge-std/Test.sol";
 
 import "solmate/tokens/ERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/interfaces/IERC2612.sol";
 
+import './SigUtils.sol';
 import '../src/GasGateway.sol';
 import '../src/GasStation.sol';
 import '../src/Interfaces.sol';
@@ -14,23 +16,21 @@ contract IntegrationTest is Test {
 
   uint256 constant DEPOSIT_VALUE = 1 ether;
   uint256 constant GAS_STATION_ETH_BALANCE = 10 ether;
-  IERC20 constant usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-  address constant WALLET = address(0x1);
+  ERC20 constant usdc = ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+  address constant USDC_WHALE = address(0xDa9CE944a37d218c3302F6B82a094844C6ECEb17);
   address constant PRICE_ORACLE = address(0x2);
+  uint256 WALLET_PRIVATE_KEY = 0xA11CE;
+  address wallet;
   IPriceOracle priceOracle;
-
-
-  //PriceOracle priceOracle;
+  SigUtils sigUtils;
   GasGateway gasGateway;
   GasStation gasStation;
 
   function setUp() public {
-
-    // Deploy
     bytes memory bytecode = abi.encodePacked(vm.getCode("PriceOracle.sol"));
     address deployed;
     assembly {
-        deployed := create(0, add(bytecode, 0x20), mload(bytecode))
+      deployed := create(0, add(bytecode, 0x20), mload(bytecode))
     }
     priceOracle = IPriceOracle(deployed);
 
@@ -41,10 +41,31 @@ contract IntegrationTest is Test {
     gasStation = new GasStation{value: GAS_STATION_ETH_BALANCE}(tokens, 500, 180, "apiRoot");
     gasStation.register{value: DEPOSIT_VALUE}(gasGateway);
 
-    payable(WALLET).sendValue(2 ether);
+
+    sigUtils = new SigUtils(usdc.DOMAIN_SEPARATOR());
+    wallet = vm.addr(WALLET_PRIVATE_KEY);
   }
 
   function test_shouldExchangeUsdcToEth() public {
-    //gasStation.exchange();
+    vm.prank(USDC_WHALE);
+    usdc.transfer(wallet, 150e6);
+    SigUtils.Permit memory permit = SigUtils.Permit({
+        owner: wallet,
+        spender: address(gasGateway),
+        value: 100e6,
+        nonce: 0,
+        deadline: block.timestamp + 1 days
+    });
+
+    bytes32 digest = sigUtils.getTypedDataHash(permit);
+    (uint8 v, bytes32 r, bytes32 s) = vm.sign(WALLET_PRIVATE_KEY, digest);
+
+    uint256 gasStationEthBalanceBefore = address(gasStation).balance;
+    gasStation.exchange(wallet, IERC2612(address(usdc)), 100e6, block.timestamp + 1 days, v, r, s);
+
+    assertEq(usdc.balanceOf(wallet), 50e6);
+    assertEq(usdc.balanceOf(address(gasStation)), 100e6);
+    assertEq(wallet.balance, gasStationEthBalanceBefore - address(gasStation).balance);
+
   }
 }
